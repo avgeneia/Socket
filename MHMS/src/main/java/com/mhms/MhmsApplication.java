8ccace1180dd4db6f8a045919963264d3fe480e1
USER <USER@DESKTOP-LV3N46H>
2020-11-11 23:23:40 +0900

    2020-11-11
    webSocket 통신을 통한 단말간 메세지 전송 기능 추가

--- MHMS/src/main/java/com/mhms/MhmsApplication.java
+++ MHMS/src/main/java/com/mhms/MhmsApplication.java
@@ -2,9 +2,16 @@ package com.mhms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @SpringBootApplication
 public class MhmsApplication{
+	
+	@Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
 	
 	public static void main(String[] args) {
 		
